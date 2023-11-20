09f4a361e93ad6f3df8e1d2ca6d3aa99a6d7cc8f
gfished <151399554+gfished@users.noreply.github.com>
2023-11-20 09:57:16 +0300

    Add files via upload

---
+++ dice combinations rec.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int f(int n)
+{
+    if (n == 0) return 1;
+    if (n < 0) return 0;
+    int res = f(n-1) + f(n-2) + f(n-3) + f(n-4) + f(n-5) + f(n-6); //мемоизация???
+    return res;
+};
+
+int main()
+{
+    int n{0};
+    cin >> n;
+    cout << f(n);
+}
